aa320f76f6947e2822345ed2d83e99c99712c520
kingkongclover <anton.reijonen@gmail.com>
2026-02-02 21:15:42 +0200

    Changed wording

--- src/components/FrontPage.tsx
+++ src/components/FrontPage.tsx
@@ -84,7 +84,7 @@ export const HomePage = ({ saveJoke }: FrontPageProps) => {
                     setReload((prev) => prev + 1)
                 }}
                 >
-                Get a joke</Button>
+                Get joke</Button>
 
                 <Button variant='contained'
                 onClick={() => {
